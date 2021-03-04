f80fa5a89e4167c6a587cf6a278597badd8f4174
Paolo Tiu <paolotiu17@gmail.com>
2021-03-04 18:42:35 +0800

    new dist

--- dist/components/StepProgress/StepProgress.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-interface Props {
-    steps: number;
-    progress: {
-        [key: number]: number;
-    };
-}
-export declare const StepProgress: ({ steps, progress }: Props) => JSX.Element;
-export {};
